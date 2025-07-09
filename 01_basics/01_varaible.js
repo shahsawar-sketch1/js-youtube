6ede21ae45cc9a117c503e92a39afd5da08b6c0b
shahsawar-sketch1 <hasnainmohmand879@gmail.com>
2025-07-09 12:01:20 +0000

    data type and variable

---
+++ 01_basics/01_varaible.js
@@ -0,0 +1,14 @@
+const accountId=14455
+let accountEmail="shahsawar.@gmail.com"
+var accountPassword="12345"
+accountCity="peshawar"
+
+let accountState;
+
+// accountId=2  not allowed
+
+accountEmail="shahsawar@gmail.com"
+accountPassword="54321"
+accountCity="lahore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strick"// use js code as newer version
+
+// alert("hello")
+
+console.log(3+
+3
+);//this is not a valid 
+
+
+
+
+let name="shahsawar"
+let age =17
+let login=false
+
+//primitive data type 
+//number =>2^53
+//bigint
+//string=>""
+//boolean
+//null =>standalone value
+//undefined
+
+
+//non primitive data type
+
+//object
+
+console.log(typeof null);
+
+
+
+
