165d4def74a9a73386e62e457398ba83ca8614a8
shahsawar-sketch1 <hasnainmohmand879@gmail.com>
2025-07-09 12:33:34 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,35 @@
+let score= "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueinNumber=Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1,false=>0
+
+
+let isloggin=1
+
+let booleanisloggedin=Boolean(isloggin)
+
+// console.log(typeof booleanisloggedin);
+// console.log(booleanisloggedin);
+
+
+// 1=>ture,0=>1
+// ""=>false;"shasawar"=>true
+
+
+let someNumber=33
+
+let stringinValue=String(someNumber)
+
+console.log(stringinValue);
+
