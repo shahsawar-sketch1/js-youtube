9a959de25e69f6c0d5b1cc35fe306a22f14c0c4e
shahsawar-sketch1 <hasnainmohmand879@gmail.com>
2025-07-14 12:30:07 +0000

    array

---
+++ 02_arrays/01_Array.js
@@ -0,0 +1,45 @@
+//array
+
+let myArray = [0 , 1, 2, 3, 4, 5]
+ let Names = ["shahsawar","abuzar","mavia", "hammad"]
+
+ const myArray2 = new Array(1, 2, 3, 4)
+
+//  console.log(myArray[1]);
+// 
+
+///////////  //array methods
+
+
+//  myArray.push(6);
+
+//  myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(4));
+
+let newarray = myArray.join()//to convert into string
+//  console.log(typeof newarray);
+ 
+//  console.log(typeof myArray);
+
+
+// slice , splice 
+
+
+console.log('A ',myArray);
+console.log(myArray.slice(1,3));
+
+console.log('B ',myArray);
+
+console.log(myArray.splice(1,3));//in splice total array will cut in range form main array
+console.log('c ',myArray);
+
+
+
+
+ 
+ 
